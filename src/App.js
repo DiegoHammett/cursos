cd2e0462aee12b6342e8533f06548f6151e42113
Diego Hernandez <diego.hernandez.124@hotmail.es>
2023-04-11 20:24:22 -0600

    Register module

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Test from './Test';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Landing from './Landing';
+import Register from './Register';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Landing></Landing>} />
         <Route exact path="/test" element={<Test id='3' type={true} />} />
+        <Route exact path="/registro" element={<Register></Register>} />
       </Routes>
     </BrowserRouter>
 
