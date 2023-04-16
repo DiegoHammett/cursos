aefcce6d951d52de6d0bdd420b6c95547aed139d
Oscar G. Flores Rodas <o.rodas@outlook.com>
2023-04-16 11:15:32 -0600

    Changes

--- src/Landing/index.js
+++ src/Landing/index.js
@@ -12,16 +12,60 @@ function Landing() {
                         <div className='landing-hero_textbox'>
                             <h1 className='landing-hero_title'>Aprende, estudia y asegura tu futuro</h1>
                             <p className='landing-hero_description'>
-                                Aprovecha el poder de las plataformas y el aprendizaje digital para conseguir tu primera opción.
+                                Aprovecha el poder del aprendizaje digital y el acompañamiento de profesores para conseguir tu primera opción.
                             </p>
                             <a className='landing-hero_button' href='/'>¡Comienza ahora!</a>
-                            <a className='landing-hero_button-2' href='/'>Conoce más </a>
+                            <a className='landing-hero_button-2' href='#beneficios'>Conoce más </a>
                         </div>
                     </div>
                 </section>
 
-                <section className='landing-section2'>
-                    a
+                <section className='landing-beneficios' id='beneficios'>
+                    <div className='landing-beneficios_container'>
+                        <h1 className='landing_title'>
+                            ¿Por qué <span className='landing_title-focus'>elegirnos</span>?
+                        </h1>
+                        <p className='landing-description'>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        </p>
+                        <div className='beneficios_boxes-container'>
+                            <div className='beneficios_box-1'>
+                                <div className='box-1_content'>
+                                    <div className='box-1_icon'><i class='bx bx-cube'></i></div>
+                                    <div className='box-1_text'>
+                                        <h3>Beneficio #1</h3>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                                        <a href='/'>Ver más</a>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className='beneficios_box-1'>
+                                <div className='box-1_content'>
+                                    <div className='box-1_icon'><i class='bx bx-cube'></i></div>
+                                    <div className='box-1_text'>
+                                        <h3>Beneficio #2</h3>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                                        <a href='/'>Ver más</a>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className='beneficios_box-1'>
+                                <div className='box-1_content'>
+                                    <div className='box-1_icon'><i class='bx bx-cube'></i></div>
+                                    <div className='box-1_text'>
+                                        <h3>Beneficio #3</h3>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                                        <a href='/'>Ver más</a>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+
+
                 </section>
             </div>
         </div>
