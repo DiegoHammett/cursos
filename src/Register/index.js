6a30a32e9c16daf76582101805252fedb3f6d8c3
Oscar G. Flores Rodas <o.rodas@outlook.com>
2023-04-17 11:59:14 -0600

    Register changes

--- src/Register/index.js
+++ src/Register/index.js
@@ -125,13 +125,6 @@ function Register() {
                                     />
                                 </div>
 
-                                {/* <div className='error-msg_label' hidden={logAlert}>
-                                    <i class='bx bxs-error-circle bx-flashing' ></i>
-                                    <label>
-                                        <b>Error</b>·<span>Usuario o contraseña incorrectos.</span>
-                                    </label>
-                                </div> */}
-
                             </div>
                         </form>
 
@@ -146,24 +139,34 @@ function Register() {
                                     <label className='input-label'>Crea una contraseña</label>
                                     <input
                                         className='input-text'
-                                        name="nombre"
-                                        type='text'
-                                        required
+                                        name="password"
                                         onChange={handleChange}
-                                        id="inputName" />
-
+                                        type="password"
+                                        id="inputPassword1"
+                                        required
+                                    />
+                                    <p className='error-msg_label' hidden={!passAlert}>
+                                        <label>
+                                            <i class='bx bx-x bx-tada' ></i><span>Las contraseñas no coinciden</span>
+                                        </label>
+                                    </p>
                                 </div>
 
                                 <div className='register-form_item'>
                                     <label className='input-label'>Confirma la contraseña</label>
                                     <input
                                         className='input-text'
-                                        name="apellido_p"
-                                        type='text'
+                                        onChange={handleCheckPassword}
+                                        type="password"
+                                        id="inputPassword2"
                                         required
-                                        onChange={handleChange}
-                                        id="inputLName1"
                                     />
+                                    <p className='error-msg_label' hidden={!passAlert}>
+                                        <label>
+                                            <i class='bx bx-x bx-tada' ></i><span>Las contraseñas no coinciden</span>
+                                        </label>
+
+                                    </p>
                                 </div>
 
                             </div>
@@ -201,73 +204,6 @@ function Register() {
             </div>
 
 
-
-
-
-            <header><h1>Registro</h1></header>
-            <form action='#'>
-                <div>
-                    <h3>Información personal</h3>
-                    <div>
-                        <label>Nombre(s) *</label>
-                        <input name="nombre" type='text' placeholder='Nombre(s)' required onChange={handleChange} id="inputName"></input>
-                    </div>
-                    <div>
-                        <div>
-                            <label>Apellido paterno *</label>
-                            <input name="apellido_p" type='text' placeholder='Apellido paterno' required onChange={handleChange} id="inputLName1"></input>
-                        </div>
-                        <div>
-                            <label>Apellido materno *</label>
-                            <input name="apellido_m" type='text' placeholder='Apellido materno' required onChange={handleChange} id="inputLName2"></input>
-                        </div>
-                    </div>
-                    <div>
-                        <div>
-                            <label>Correo electrónico *</label>
-                            <input name="email" type='email' placeholder='Ingrese su correo electrónico' required onChange={handleChange} id="inputEmail" aria-describedby="emailHelp"></input>
-                            <p hidden={!emailAlert}>Introduce una dirección de correo electrónico válida</p>
-                        </div>
-                    </div>
-                </div>
-
-                <div>
-                    <h3>Paquete</h3>
-                    <label>Selecciona el paquete que deseas contratar</label>
-                    <div>
-
-                    </div>
-                </div>
-
-                <div>
-                    <h3>Información de la plataforma</h3>
-                    <div>
-                        <label htmlFor="inputPassword1">Cree una contraseña *</label>
-                        <input placeholder='Ingrese una contraseña' name="password" onChange={handleChange} type="password" id="inputPassword1" required></input>
-                        <p hidden={!passAlert}>Las contraseñas no coinciden</p>
-                    </div>
-                    <div>
-                        <label>Confirmar contraseña *</label>
-                        <input placeholder='Confirme la contraseña' onChange={handleCheckPassword} type="password" id="inputPassword2" required></input>
-                    </div>
-                    <p hidden={!passAlert}>Las contraseñas no coinciden</p>
-
-                </div>
-
-                <h6 hidden={!fillAlert}>Debes llenar todos los campos (*)</h6>
-                <div>
-                    <label>Finalizar pre-registro</label>
-                    {!!ok && <label>Registro exitoso</label>}
-                    {!!dupEmail && <label>Email duplicado</label>}
-                    {!!error && <label>Error</label>}
-                    <button type="button" onClick={handleSubmit}>Enviar</button>
-                    {/* <CustomModal id="Modal" message="¡Tus documentos han sido enviados correctamente! Inicia sesión para ver más detalles." route="/login"></CustomModal>
-                        <CustomModal id="Boleta" message="La boleta introducida ya está registrada en la plataforma"></CustomModal>
-                        <CustomModal id="Email" message="El email introducido ya está registrado en la plataforma"></CustomModal>
-                        <CustomModal id="Seminario" message="En estos momentos el pre-registro se encuentra deshabilitado. Intenta de nuevo más tarde." route="/"></CustomModal>
-                        <ErrorMessage show={error} /> */}
-                </div>
-            </form>
         </React.Fragment>
     );
 }
