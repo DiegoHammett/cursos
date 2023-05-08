0b93ca7408c50cb2b6c0920d11b958c349655b9c
Diego Hernandez <diego.hernandez.124@hotmail.es>
2023-05-07 19:09:12 -0600

    API

--- api/index.php
+++ api/index.php
@@ -3,6 +3,7 @@ include "config.php";
 include "utils.php";
 
 header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
 
 $dbConn =  connect($db);
 
@@ -12,6 +13,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET')
     if (isset($_GET['column'])) $query = $query.$_GET['column']; else $query = $query."*";
     $query = $query." FROM ".$_GET['table'];
     if (isset($_GET['where'])) $query = $query." WHERE ".$_GET['where'];
+    if (isset($_GET['extra'])) $query = $query." ".$_GET['extra'];
     $sql = $dbConn->prepare($query);
     try{
       $sql->execute();
@@ -19,12 +21,13 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET')
       header("HTTP/1.1 200 OK");
       echo json_encode($sql->fetchAll());
 	}catch (Exception $e){
-        echo $e;
+        $arr = array('error' => $e);
+        echo json_encode($arr);
     }
     exit();
 }
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST')
+elseif ($_SERVER['REQUEST_METHOD'] == 'POST')
 {
     $input = $_POST;
     $fields = getParams($input);
@@ -44,7 +47,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
         $row = $sql->fetch(PDO::FETCH_NUM);
         $IDINSERTED = json_encode($row[0]);
     }catch (Exception $e){
-        echo $e;
+        $arr = array('error' => $e);
+        echo json_encode($arr);
     }
     foreach($_FILES as $param => $value)
     {
@@ -56,36 +60,40 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
     exit();
 }
 
-if ($_SERVER['REQUEST_METHOD'] == 'DELETE')
+elseif ($_SERVER['REQUEST_METHOD'] == "DELETE")
 {
-    echo "METODO DELETE";
-/*    
-	$id = $_GET['id'];
-  $statement = $dbConn->prepare("DELETE FROM posts where id=:id");
-  $statement->bindValue(':id', $id);
-  $statement->execute();
-	header("HTTP/1.1 200 OK");
-	exit();
-	*/
+    $query = "DELETE FROM ".$_GET['table']." WHERE id=".$_GET['id'];
+    $sql = $dbConn->prepare($query);
+    try{
+        $sql->execute();
+        $arr = array('status' => 'OK');
+        echo json_encode($arr);
+    }catch (Exception $e){
+        $arr = array('error' => $e);
+        echo json_encode($arr);
+    }
+    header("HTTP/1.1 200 OK");
+    exit();
 }
 
-if ($_SERVER['REQUEST_METHOD'] == 'PUT')
+elseif ($_SERVER['REQUEST_METHOD'] == 'PUT')
 {
-    $input = $_PUT;
+    echo "PUT";
+    header("HTTP/1.1 200 OK");
+    exit();
+    /*$input = $_PUT;
     $datosPUT = fopen("php://input", "r");
     echo $datosPUT;
     $fields = getParams($input);
     $sql = "UPDATE ".$_GET['table']."
           SET $fields
           WHERE ".$_GET['column']."=".$_GET['id'];
-    /*$statement = $dbConn->prepare($sql);
+    $statement = $dbConn->prepare($sql);
     bindAllValues($statement, $input);
-    $statement->execute();*/
+    $statement->execute();
     echo $sql;
     header("HTTP/1.1 200 OK");
-    exit();
+    exit();*/
 }
-
-header("HTTP/1.1 400 Bad Request");
 
 ?>
