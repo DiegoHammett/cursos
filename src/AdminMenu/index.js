46538bb585e678dfec08dd5c316e696c4a271dd2
Oscar G. Flores Rodas <o.rodas@outlook.com>
2023-06-19 12:51:38 -0600

    Revert "Revert "Changes""
    This reverts commit 608608084e674845ff4f5c73bdac91926d6a1513.

--- src/AdminMenu/index.js
+++ src/AdminMenu/index.js
@@ -5,6 +5,8 @@ import DarkMode from '../DarkMode';
 import UserImg from './user.png';
 import './adminmenu_styles.css'
 
+import { Tabs, TabList, Tab, TabPanel } from "https://cdn.skypack.dev/react-tabs@3.2.2";
+
 function AdminMenu({ email }) {
 
     const [admin, setAdmin] = useState([])
@@ -49,178 +51,141 @@ function AdminMenu({ email }) {
         <React.Fragment>
 
             <div className='dashboard_body'>
-
-
-                <div className={sidebarActive ? 'dashboard_menu-sidebar active' : 'dashboard_menu-sidebar'}>
-                    <div className='d_m-s_top'>
-                        <div className='d_m-s_top-logo-container'>
-                        </div>
-                        <i className='d_m-s_top-logo bx bx-menu' id='btn-menu' onClick={handleSidebarClick}></i>
-                        <div className='d_m-s_top-user'>
-                            <img className='d_m-s_top-user-img' src={UserImg} alt='me' />
-                            <div className='d_m-s_top-user-info'>
-                                <span className='top-user-name'><b>{admin.nombre}</b></span>
-                                <div className='top-user-plan-container'>
-                                    <label className='lbl'>RANGO</label>
-                                    <span className='top-user-plan'><b className='b-medium'>{admin.nombre_plan}</b></span>
-                                </div>
+                <Tabs>
+                    <div className={sidebarActive ? 'dashboard_menu-sidebar active' : 'dashboard_menu-sidebar'}>
+                        <div className='d_m-s_top'>
+                            <div className='d_m-s_top-logo-container'>
                             </div>
-                        </div>
-                    </div>
-
-                    <div className='d_m-s_menu'>
-                        <ul>
-                            <li>
-                                <a href='#'>
-                                    <i class='bx bxs-home'></i>
-                                    <span className='d_m-s_menu-item'>Inicio</span>
-                                </a>
-                                <span className='d_m-s_menu-tooltip'>Inicio</span>
-                            </li>
-                            <li>
-                                <a href='#'>
-                                    <i class='bx bx-cube'></i>
-                                    <span className='d_m-s_menu-item'>Item</span>
-                                </a>
-                                <span className='d_m-s_menu-tooltip'>Item</span>
-                            </li>
-                            <li>
-                                <a href='#'>
-                                    <i class='bx bx-cube'></i>
-                                    <span className='d_m-s_menu-item'>Item</span>
-                                </a>
-                                <span className='d_m-s_menu-tooltip'>Item</span>
-                            </li>
-                            <li>
-                                <a href='#'>
-                                    <i class='bx bx-cube'></i>
-                                    <span className='d_m-s_menu-item'>Item</span>
-                                </a>
-                                <span className='d_m-s_menu-tooltip'>Item</span>
-                            </li>
-                            <li>
-                                <a href='#'>
-                                    <i class='bx bx-cube'></i>
-                                    <span className='d_m-s_menu-item'>Item</span>
-                                </a>
-                                <span className='d_m-s_menu-tooltip'>Item</span>
-                            </li>
-                            <li>
-                                <a href='#'>
-                                    <i class='bx bx-cube'></i>
-                                    <span className='d_m-s_menu-item'>Item</span>
-                                </a>
-                                <span className='d_m-s_menu-tooltip'>Item</span>
-                            </li>
-                            <li>
-                                <a href='#'>
-                                    <i class='bx bx-cube'></i>
-                                    <span className='d_m-s_menu-item'>Item</span>
-                                </a>
-                                <span className='d_m-s_menu-tooltip'>Item</span>
-                            </li>
-
-                        </ul>
-                    </div>
-
-                    <div className='d_m-s_bottom'>
-
-                    </div>
-                </div>
-
-                <div className='dashboard_main'>
-                    <div className='dashboard_menu-header'>
-                        <div className='d_m-h_container'>
-                            <div className='d_m-h_logo'>
-                                <a className='d_m-h-logo-item' href='/'>
-                                    <i className='bx bx-cube'> </i><span>Logo</span>
-                                </a>
-                            </div>
-                            <div className='d_m-h_options'>
-                                <ul className='d_m-h_navbar-links'>
-                                    <li className='navbar-links_item'>
-                                        <a href='/'>#Item1</a>
-                                    </li>
-                                    <li className='navbar-links_item'>
-                                        <a href='/'>#Item2</a>
-                                    </li>
-                                    <li className='navbar-links_item'>
-                                        <a href='/'>#Item3</a>
-                                    </li>
-                                </ul>
-                                <div className='d_m-h_navbar_session-btns'>
-                                    <div className='d_m-h-darkmode '>
-                                        <DarkMode />
+                            <i className='d_m-s_top-logo bx bx-menu' id='btn-menu' onClick={handleSidebarClick}></i>
+                            <div className='d_m-s_top-user'>
+                                <img className='d_m-s_top-user-img' src={UserImg} alt='me' />
+                                <div className='d_m-s_top-user-info'>
+                                    <span className='top-user-name'><b>{admin.nombre}</b></span>
+                                    <div className='top-user-plan-container'>
+                                        <label className='lbl'>RANGO</label>
+                                        <span className='top-user-plan'><b className='b-medium'>{admin.nombre_plan}</b></span>
                                     </div>
-                                    <button className='btn-xs'>Cerrar sesión</button>
                                 </div>
                             </div>
+                        </div>
 
+                        <div className='d_m-s_menu'>
+                            <TabList className="mt--10">
+                                <Tab>
+                                    <button>
+                                        <i class='bx bxs-home'></i>
+                                        <span className='d_m-s_menu-item'>Inicio</span>
+                                    </button>
+                                    <span className='d_m-s_menu-tooltip'>Inicio</span>
+                                </Tab>
+                                <Tab>
+                                    <button href='#'>
+                                        <i class='bx bxs-edit'></i>
+                                        <span className='d_m-s_menu-item'>Editar</span>
+                                    </button>
+                                    <span className='d_m-s_menu-tooltip'>Editar</span>
+                                </Tab>
+                            </TabList>
+                        </div>
+
+                        <div className='d_m-s_bottom'>
+
+                        </div>
+                    </div>
+
+                    <div className='dashboard_main'>
+                        <div className='dashboard_menu-header'>
+                            <div className='d_m-h_container'>
+                                <div className='d_m-h_logo'>
+                                    <a className='d_m-h-logo-item' href='/'>
+                                        <i className='bx bx-cube'> </i><span>Logo</span>
+                                    </a>
+                                </div>
+                                <div className='d_m-h_options'>
+                                    <ul className='d_m-h_navbar-links'>
+                                        <li className='navbar-links_item'>
+
+                                        </li>
+                                    </ul>
+                                    <div className='d_m-h_navbar_session-btns'>
+                                        <div className='d_m-h-darkmode '>
+                                            <DarkMode />
+                                        </div>
+                                        <button className='btn-xs'>Cerrar sesión</button>
+                                    </div>
+                                </div>
+
+                            </div>
+
+                        </div>
+
+                        <div className='dashboard_main_container '>
+                            <TabPanel className='cursos-container'>
+                                <div className='tabs'>
+                                    <Tabbs>
+                                        <Tabb label="Cursos">
+                                            <section className='cursos-container'>
+                                                <div className='cursos-container-header'>
+                                                    <h1>Cursos</h1>
+                                                    <button className='btn'><i class='bx bx-plus-circle icon'></i>Agregar curso</button>
+                                                </div>
+
+                                                {asignaturas.map(asignatura => (
+                                                    <div className='cursos-container-item' key={asignatura.id}>
+                                                        <div class="three">
+                                                            <h1>{asignatura.nombre}</h1>
+                                                        </div>
+                                                        <div className='courseCards inset'>
+                                                            <CourseList id={asignatura.id} admin={true} />
+                                                        </div>
+
+                                                    </div>
+                                                ))}
+                                            </section>
+                                        </Tabb>
+                                        <Tabb label="Simuladores">
+                                            <section className='cursos-container'>
+                                                <div className='cursos-container-header'>
+                                                    <h1>Simuladores</h1>
+                                                    <button className='btn'><i class='bx bx-plus-circle icon'></i>Crear simulador</button>
+                                                </div>
+
+
+                                                {simuladores.length > 0 && simuladores.map(simulador => (
+                                                    <div key={simulador.id}>
+                                                        <div class="three">
+                                                            <h1>{simulador.nombre}</h1>
+                                                        </div>
+                                                    </div>
+                                                ))}
+                                            </section>
+                                        </Tabb>
+                                    </Tabbs>
+                                </div>
+                            </TabPanel>
+                            <TabPanel className='cursos-container'>
+                                <section className='cursos-container'>
+                                    <div className='cursos-container-header'>
+                                        <h1>Editar</h1>
+                                    </div>
+                                    <div>
+                                        <p>-- Aquí debería cargarse el componente de Editar --</p>
+                                    </div>
+                                </section>
+                            </TabPanel>
 
                         </div>
 
                     </div>
-
-                    <div className='dashboard_main_container '>
-                        <div className='tabs'>
-                            <Tabs>
-                                <Tab label="Cursos">
-                                    <section className='cursos-container'>
-                                        <div className='cursos-container-header'>
-                                            <h1>Cursos</h1>
-                                            <button className='btn'><i class='bx bx-plus-circle icon'></i>Agregar curso</button>
-                                        </div>
-
-                                        {asignaturas.map(asignatura => (
-                                            <div className='cursos-container-item' key={asignatura.id}>
-                                                <div class="three">
-                                                    <h1>{asignatura.nombre}</h1>
-                                                </div>
-                                                <div className='courseCards inset'>
-                                                    <CourseList id={asignatura.id} admin={true} />
-                                                </div>
-
-                                            </div>
-                                        ))}
-                                    </section>
-                                </Tab>
-                                <Tab label="Simuladores">
-                                    <section className='cursos-container'>
-                                        <div className='cursos-container-header'>
-                                            <h1>Simuladores</h1>
-                                            <button className='btn'><i class='bx bx-plus-circle icon'></i>Crear simulador</button>
-                                        </div>
-
-
-                                        {simuladores.length > 0 && simuladores.map(simulador => (
-                                            <div key={simulador.id}>
-                                                <div class="three">
-                                                    <h1>{simulador.nombre}</h1>
-                                                </div>
-                                            </div>
-                                        ))}
-                                    </section>
-                                </Tab>
-                            </Tabs>
-                        </div>
-
-
-
-
-
-
-
-                    </div>
-
-                </div>
+                </Tabs>
             </div>
 
-          
+
         </React.Fragment>
     )
 }
 
-class Tabs extends React.Component {
+class Tabbs extends React.Component {
     state = {
         activeTab: this.props.children[0].props.label
     }
@@ -258,7 +223,7 @@ const TabButtons = ({ buttons, changeTab, activeTab }) => {
     )
 }
 
-const Tab = props => {
+const Tabb = props => {
     return (
         <React.Fragment>
             {props.children}
