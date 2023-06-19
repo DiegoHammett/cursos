85878398bbefdc318216d6606a85748272bef77b
Oscar G. Flores Rodas <o.rodas@outlook.com>
2023-06-19 14:28:00 -0600

    Componente 'Math': Listo

--- src/MathQ/TextSymbols.js
+++ src/MathQ/TextSymbols.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import {MathSymbol} from 'brainly-style-guide';
-import {symbols} from './symbols';
-import MathExp from '../MathExp';
+import { symbols } from './symbols';
+import { StaticMathField } from 'react-mathquill'
 
-export default function Symbols({onClick}) {
+export default function Symbols({ onClick }) {
   const buttonNodes = symbols.map(symbol => (
-    <button type="button" key={symbol.icon} className="tex-button" onClick={() => onClick(symbol.data)}>
-      {symbol.icon}
+    <button type="button" key={symbol.icon} className="tex-button it-inset-shadow " onClick={() => onClick(symbol.data)}>
+      <StaticMathField>{symbol.data}</StaticMathField>
     </button>
   ));
 
   return (
-    <div style={{display: 'grid', gridAutoFlow: 'column',gridTemplateRows: 'auto auto', gridGap: 5}}>
+    <div className='math-btns-grid'>
       {buttonNodes}
     </div>
   )
--- src/MathQ/index.js
+++ src/MathQ/index.js
@@ -1,32 +1,29 @@
+
+
 import React, { useState } from 'react'
-import { addStyles, EditableMathField, StaticMathField } from 'react-mathquill'
+import { addStyles, EditableMathField } from 'react-mathquill'
 import TextSymbols from './TextSymbols'
 import './style.css';
-import MathExp from '../MathExp';
 
 addStyles()
 
-function MathQ() {
-    const [latex, setLatex] = useState('\\frac{1}{\\sqrt{2}}\\cdot 2')
+function MathQ({ exp }) {
+    const [latex, setLatex] = useState(exp)
 
     function handleInsertSymbol(symbol) {
         setLatex(oldFormula => oldFormula + symbol);
     }
 
     return (
-        <div>
+        <div className='math-body inset'>
             <EditableMathField
                 latex={latex}
                 onChange={(mathField) => {
                     setLatex(mathField.latex())
                 }}
             />
-            <StaticMathField>{latex}</StaticMathField>
-            
             <TextSymbols onClick={handleInsertSymbol}></TextSymbols>
-            <MathExp text={latex}/>
         </div>
-
     )
 
 }
